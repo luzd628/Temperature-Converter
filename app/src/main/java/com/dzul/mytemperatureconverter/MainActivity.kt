78539dc5bd8df347d70aa406a1400c4ef746d87a
luzd <dzulfikri628@gmail.com>
2023-05-23 14:56:17 +0700

    Second Commit(Statefull Converter)

--- app/src/main/java/com/dzul/mytemperatureconverter/MainActivity.kt
+++ app/src/main/java/com/dzul/mytemperatureconverter/MainActivity.kt
@@ -3,13 +3,24 @@ package com.dzul.mytemperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dzul.mytemperatureconverter.ui.theme.MyTemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    StatefulTemperaturInput()
                 }
             }
         }
@@ -34,10 +45,40 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun StatefulTemperaturInput(
+    modifier: Modifier = Modifier,
+){
+    //var input =""
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = {newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit,output))
+    }
+}
+
+fun convertToFahrenheit(celcius: String): String =
+    celcius.toDoubleOrNull()?.let {
+        (it * 9/5) + 32
+    }.toString()
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyTemperatureConverterTheme {
-        Greeting("Android")
+        StatefulTemperaturInput()
     }
 }
